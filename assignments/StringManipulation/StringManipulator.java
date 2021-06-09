1685a165c22967501eafa16b99639d91fde9171d
blake-gifford <blakegifford01@gmail.com>
2021-06-08 21:32:53 -0700

    work

--- assignments/StringManipulation/StringManipulator.java
+++ assignments/StringManipulation/StringManipulator.java
@@ -1,8 +1,18 @@
 public class StringManipulator {
-    public String trimAndConcat(String str1, String str2){
-        return str1.trim() + str2.trim();
-    }
-    public Integer getIndexOrNull(String str, char ch1){
-        
+    // public String trimAndConcat(String str1, String str2){
+    //     return str1.trim() + str2.trim();
+    // }
+    // public Integer getIndexOrNull(String str, char ch1){
+    //     Integer result = str.indexOf(ch1);
+    //     if(result < 0){
+    //         result = null;
+    //     }
+    //     return result;
+    // }
+    // public Integer getIndexOrNull(String str, String char){
+    //     return str.indexOf(char);
+    // }
+    public String concatSubstring(String str1, Integer int1, Integer int2, String str2){
+        return str1.indexOf(int1) + str2;
     }
 }
--- assignments/StringManipulation/StringManipulatorTest.java
+++ assignments/StringManipulation/StringManipulatorTest.java
@@ -1,19 +1,19 @@
 public class StringManipulatorTest {
     public static void main(String[] args){
-        StringManipulator manipulator = new StringManipulator();
-        String str = manipulator.trimAndConcat("    Hello    ","    World    ");
-        System.out.println(str);
+        // StringManipulator manipulator = new StringManipulator();
+        // String str = manipulator.trimAndConcat("    Hello    ","    World    ");
+        // System.out.println(str);
 
 
 
-        StringManipulator manipulator = new StringManipulator();
-        char letter = 'o';
-        Integer a = manipulator.getIndexOrNull("Coding", letter);
-        Integer b = manipulator.getIndexOrNull("Hello World", letter);
-        Integer c = manipulator.getIndexOrNull("Hi", letter);
-        System.out.println(a); // 1
-        System.out.println(b); // 4
-        System.out.println(c); // null
+        // StringManipulator manipulator = new StringManipulator();
+        // char letter = 'o';
+        // Integer a = manipulator.getIndexOrNull("Coding", letter);
+        // Integer b = manipulator.getIndexOrNull("Hello World", letter);
+        // Integer c = manipulator.getIndexOrNull("Hi", letter);
+        // System.out.println(a); // 1
+        // System.out.println(b); // 4
+        // System.out.println(c); // null
 
 
 
@@ -28,9 +28,9 @@ public class StringManipulatorTest {
 
 
 
-        // StringManipulator manipulator = new StringManipulator();
-        // String word = manipulator.concatSubstring("Hello", 1, 2, "world");
-        // System.out.println(word); // eworld
+        StringManipulator manipulator = new StringManipulator();
+        String word = manipulator.concatSubstring("Hello", 1, 2, "world");
+        System.out.println(word); // eworld
 
     }
 
