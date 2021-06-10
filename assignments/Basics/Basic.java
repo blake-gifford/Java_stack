98fa914989054738157fa74b12398e89e890b62b
blake-gifford <blakegifford01@gmail.com>
2021-06-09 20:33:14 -0700

    daily work

--- assignments/Basics/Basic.java
+++ assignments/Basics/Basic.java
@@ -1,5 +1,74 @@
 public class Basic {
-    public Integer oneToOneHundred(int number){
-        return number;
-    }
-}
+    // public void oneToOneHundred(){
+    //     for(int i = 0; i < 256; i++){
+    //         System.out.println(i);
+    //     }
+    // }
+    // public void oddToOneHundred(){
+    //     for(int i = 1; i < 256; i+=2){
+    //         System.out.println(i);
+    //     }
+    // }
+    // public void evenToOneHundred(){
+    //     for(int i = 0; i < 101; i += 2){
+    //         System.out.println(i);
+    //     }
+    // }
+    // public void sumTo255(){
+    //     for(int i = 0; i < 256; i++){
+    //         int result = i+=i+1;
+    //         System.out.println(result);
+    //     }
+    // }
+    // public void iteratingThroughAnArray(int[] myArray){
+    //     for(int i = 0; i < myArray.length; i++){
+    //         System.out.println(myArray[i]);
+    //     }
+    // }
+    // public void findMaxInArray(int[] myArray){
+            // int max = myArray[0];
+    //     for(int i = 0; i < myArray.length; i++){
+    //         if(myArray[i] > max){
+    //             max = myArray[i];
+    //             // return max;
+    //             System.out.println(max);
+    //         }
+    //     }
+    // }
+    // public void getAverage(int[] myArray){
+    //     int sum = 0;
+    //     for(int i = 0; i < myArray.length; i++){
+    //         sum =+ myArray[i];
+    //     }
+    //     double avg = sum / myArray.length;
+    //     System.out.println(avg);
+    // }
+    // public ArrayList <Integer> arrayWithOddNums(){
+    //     ArrayList<Integer> list = new ArrayList<Integer>();
+
+    //     for(int i = 0; i < 256; i++){
+    //         if(i % 2 != 0){
+    //             list.add(i);
+    //         }
+    //     }
+    //     return list;
+    // }
+    // public void greaterThanY(int[] myArray){
+    //     // int[] myArray = {2,4,5,7,6,3,9};
+    //     int y = 4;
+
+    //     for(int i = 0; i < myArray.length; i++){
+    //         if(myArray[i] > y){
+    //             System.out.println(myArray[i]);
+    //         }
+    //     }
+    // }
+    // public ArrayList<Integer> squaredArray(){
+    //     ArrayList<Integer> list = new ArrayList<Integer>();
+    //     int size = myArray.size();
+    //     for(int i = 0; i < size; i++){
+    //         int squared = myArray[i] * myArray[i];
+    //         return squared;
+    //     }
+    // }
+}  
--- assignments/Basics/BasicTesting.java
+++ assignments/Basics/BasicTesting.java
@@ -1,9 +1,47 @@
+import java.util.ArrayList;
 public class BasicTesting {
-    public static void main(String[] args){
-        oneToOneHundred o = new Basic();
-        for(int i = 0; i < 101; i++){
-            Integer result = o.oneToOneHundred(i);
-            System.out.println(result);
-        }
-    }
+    // public static void main(String[] args){
+    //     Basic o = new Basic();
+    //     o.oneToOneHundred();
+    // }
+    // public static void main(String[] args){
+    //     Basic odd = new Basic();
+    //     odd.oddToOneHundred();
+    // }
+    // public static void main(String[] args){
+    //     Basic even = new Basic();
+    //     even.evenToOneHundred();
+    // }
+    // public static void main(String[] args){
+    //     Basic sum = new Basic();
+    //     sum.sumTo255();
+    // }
+    // public static void main(String[] args){
+    //     int[] myArray = {1,3,5,7,9,13};
+    //     Basic array = new Basic();
+    //     array.iteratingThroughAnArray(myArray);
+    // }
+    // public static void main(String[] args){
+    //     int[] myArray = {1,5,8,54,8};
+    //     Basic array = new Basic();
+    //     array.findMaxInArray(myArray);
+    // }
+    // public static void main(String[] args){
+    //     int[] myArray = {1,4,6,7,9};
+    //     Basic array = new Basic();
+    //     array.getAverage(myArray);
+    // }
+    // public static void main(String[] args){
+    //     Basic b = new Basic();
+    //     System.out.println(b.arrayWithOddNums());
+    // }
+    // public static void main(String[] args){
+    //     int[] myArray = {2,4,5,7,6,3,9};
+    //     Basic b = new Basic();
+    //     System.out.println(myArray);
+    // }
+    // public static void main(String[] args){
+    //     Basic b = new Basic();
+    //     System.out.println(b.squaredArray(myArray));
+    // }
 }
